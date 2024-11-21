836539b23cd70077929dbe49f9c865211593a946
elroi carmel <elroica277@gmail.com>
2024-11-21 12:57:15 +0200

    List all processes

--- Ex1/main.py
+++ Ex1/main.py
@@ -1,38 +1,49 @@
 import os, pwd, sys, grp
 
 PROC_DIR = "/proc"
+FIELDS= ['Pid', 'PPid', 'Uid', 'Uname', 'Gid', 'Gname', 'State', 'Name']
 
 
-with open('dummy.txt', 'w') as details:
-    ps_infos = []
-    for pid in os.listdir(PROC_DIR):
-        if pid.isdigit():
-            # print(pid)
-            status_file = os.path.join(PROC_DIR, pid, 'status')
-            with open(status_file, 'r') as f:
-                lines = f.readlines()
-                process_info = {}
-                for line in lines:
-                    if line.startswith('Pid'):
-                        process_info['Pid'] = line.split()[1]
-                    elif line.startswith('PPid:'):
-                        process_info['PPid'] = line.split()[1]
-                    elif line.startswith('Uid:'):
-                        process_info['Uid'] = line.split()[1]
-                        uname = pwd.getpwuid(int(process_info['Uid'])).pw_name
-                        process_info['Uname'] = uname
-                    elif line.startswith('Gid:'):
-                        process_info['Gid'] = line.split()[1]
-                        gname = grp.getgrgid(int(process_info['Gid'])).gr_name
-                        process_info['Gname'] = gname
-                    elif line.startswith('State:'):
-                        process_info['State'] = line.split()[1]
-                    elif line.startswith('Name'):
-                        process_info['CommandName'] = line.split()[1]
-                ps_infos.append(process_info)
-
-    details.write(str(ps_infos))
-print(ps_infos[:4])
+def pcb_to_entry(pcb: dict) -> str:
+    attr = []
+    for field in FIELDS:
+        attr.append(pcb[field])
+    return '\t'.join(attr)
 
 
+def main():
 
+    with open('det.txt', 'w') as details:
+        col = list(FIELDS)
+        col[-1] = 'CommandName'
+        details.write(f'{'\t'.join(col)}\n')
+        for pid in os.listdir(PROC_DIR):
+            if pid.isdigit():
+                # print(pid)
+                status_file = os.path.join(PROC_DIR, pid, 'status')
+                with open(status_file, 'r') as f:
+                    lines = f.readlines()
+                    process_info = {}
+                    for line in lines:
+                        if line.startswith('Pid:'):
+                            process_info['Pid'] = line.split()[1]
+                        elif line.startswith('PPid:'):
+                            process_info['PPid'] = line.split()[1]
+                        elif line.startswith('Uid:'):
+                            process_info['Uid'] = line.split()[1]
+                            uname = pwd.getpwuid(int(process_info['Uid'])).pw_name
+                            process_info['Uname'] = uname
+                        elif line.startswith('Gid:'):
+                            process_info['Gid'] = line.split()[1]
+                            gname = grp.getgrgid(int(process_info['Gid'])).gr_name
+                            process_info['Gname'] = gname
+                        elif line.startswith('State:'):
+                            process_info['State'] = line.split()[1]
+                        elif line.startswith('Name'):
+                            process_info['Name'] = line.split()[1]
+                    details.write(f'{pcb_to_entry(process_info)}\n')
+    
+
+
+if __name__ == '__main__':
+    main()
--- Ex1/tweeking.py
+++ Ex1/tweeking.py
@@ -1,14 +1,4 @@
 import pwd
 import sys
 
-val = sys.argv
-print(val)
-
-# Replace with the UID you want to look up
-uid = 1000
-
-# Get the username associated with the UID
-user_info = pwd.getpwuid(uid)
-
-# Print the username
-print(user_info.pw_name)
+raise Exception("hello")
