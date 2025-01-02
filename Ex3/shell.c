91a529a31ba69e97efd1b8a26b8555c11cf04102
elroi carmel <elroica277@gmail.com>
2025-01-02 23:46:40 +0200

    add: piping

--- Ex3/shell.c
+++ Ex3/shell.c
@@ -7,13 +7,17 @@
 #include "unistd.h"
 #include <string.h>
 
-void executeCommand(char *command);
+void executeCommand(char *command, int isPiped);
+void executePipeline(char *pipeCommands[], int pipec);
 int parseCommand(char *command, char *argv[]);
+int parsePipeline(char *raw, char *pipes[]);
 void redirectCommand(char *argv[], int argc);
 
 int main()
 {
     char command[1024];
+    char *pipes[15];
+    int pipec;
 
     while (1)
     {
@@ -22,11 +26,18 @@ int main()
         fgets(command, 1024, stdin);
         command[strlen(command) - 1] = '\0';
 
-        executeCommand(command);
+        pipec = parsePipeline(command, pipes);
+        if (pipec > 1)
+        {
+            executePipeline(pipes, pipec);
+            continue;
+        }
+
+        executeCommand(command, 0);
     }
 }
 
-void executeCommand(char *command)
+void executeCommand(char *command, int isPiped)
 {
     char *argv[10];
     int amper = 0;
@@ -38,22 +49,32 @@ void executeCommand(char *command)
         amper = 1;
         argv[argc - 1] = NULL;
     }
-    pid_t cpid = fork();
-    if (cpid < 0)
+    if (!isPiped)
     {
-        perror("fork");
-        exit(1);
+        pid_t cpid = fork();
+        if (cpid < 0)
+        {
+            perror("fork");
+            exit(1);
+        }
+        else if (cpid == 0) // child
+        {
+            redirectCommand(argv, argc);
+            execvp(argv[0], argv);
+            perror("execvp");
+            exit(1);
+        }
+        // parent continues here
+        if (amper == 0)
+            waitpid(cpid, NULL, 0);
     }
-    else if (cpid == 0) // child
+    else
     {
         redirectCommand(argv, argc);
         execvp(argv[0], argv);
         perror("execvp");
         exit(1);
     }
-    // parent continues here
-    if (amper == 0)
-        waitpid(cpid, NULL, 0);
 }
 
 int parseCommand(char *command, char *argv[])
@@ -147,5 +168,62 @@ void redirectCommand(char *argv[], int argc)
             perror("close");
             exit(1);
         }
+    }
+}
+
+int parsePipeline(char *raw, char *pipes[])
+{
+    int count = 0;
+    char *token;
+    token = strtok(raw, "|");
+    while (token != NULL)
+    {
+        pipes[count] = token;
+        token = strtok(NULL, "|");
+        count++;
+    }
+    return count;
+}
+
+void executePipeline(char *pipeCommands[], int pipec)
+{
+    int pfd[2], i;
+    int prev_fd = -1; // Previous pipe's read-end
+
+    for (i = 0; i < pipec; i++)
+    {
+        if (pipe(pfd) < 0)
+        {
+            perror("pipe");
+            exit(1);
+        }
+
+        pid_t cpid = fork();
+        if (cpid == 0)
+        {
+            // Child process
+            if (prev_fd != -1)
+            {
+                dup2(prev_fd, STDIN_FILENO); // Redirect stdin to the previous pipe
+                close(prev_fd);
+            }
+            if (i != pipec - 1)
+            {
+                dup2(pfd[1], STDOUT_FILENO); // Redirect stdout to the current pipe
+            }
+            close(pfd[0]); // Close unused read-end of the current pipe
+            close(pfd[1]);
+
+            executeCommand(pipeCommands[i], 1);
+        }
+
+        waitpid(cpid, NULL, 0);
+        // Parent process
+        close(pfd[1]); // Close unused write-end of the current pipe
+        if (prev_fd != -1)
+        {
+            close(prev_fd); // Close the previous pipe's read-end
+        }
+        prev_fd = pfd[0]; // Update prev_fd to the current pipe's read-end
     }
 }
